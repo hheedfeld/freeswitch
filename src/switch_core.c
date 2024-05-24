d942dc4f29103fed5e70ef79c729c3b4a03d6a5e
Henning Heedfeld <heedfeld@gmail.com>
2024-05-24 17:49:54 +0200

    Update switch_core.c

--- src/switch_core.c
+++ src/switch_core.c
@@ -2418,8 +2418,8 @@ SWITCH_DECLARE(const char *) switch_core_banner(void)
 
 			"|   Anthony Minessale II, Michael Jerris, Brian West, Others  |\n"
 			"|   FreeSWITCH (http://www.freeswitch.org)                    |\n"
-			"|   Paypal Donations Appreciated: paypal@freeswitch.org       |\n"
-			"|   Brought to you by ClueCon http://www.cluecon.com/         |\n"
+			"|                                                             |\n"
+			"|   callio.net Fernsprechdienst                               |\n"
 			".=============================================================.\n"
 			"\n");
 }
